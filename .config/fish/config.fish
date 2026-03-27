e6eca5b99d791a48a12ed69256cc016667c3ef30
TripShuti <spokeosen@gmail.com>
2026-03-27 13:34:11 +0200

    Fix: upload actual config files instead of submodule

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+function fish_greeting                                            
+    # do nothing
+    fastfetch
+end 
+# Theme: bl1nk (відповідно до Kitty)
+set -g fish_color_normal A0A0A0
+set -g fish_color_command B5E354
+set -g fish_color_quote FEED6B
+set -g fish_color_redirection 9D6EFE
+set -g fish_color_end 8B54FE
+set -g fish_color_error F92571
+set -g fish_color_param 889BA1
+set -g fish_color_comment 505354
+set -g fish_color_match --background=505354
+set -g fish_color_selection --background=505354
+set -g fish_color_search_match --background=505354
+set -g fish_color_operator 8B54FE
+set -g fish_color_escape 465456
+set -g fish_color_autosuggestion 505354
+set -g fish_color_cwd 0B72C1
+set -g fish_color_user B5E354
+set -g fish_color_host A0A0A0
+set -g fish_color_status F92571
+set -g fish_color_valid_path --underline
+if status is-interactive
+    starship init fish | source
+end
+set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
